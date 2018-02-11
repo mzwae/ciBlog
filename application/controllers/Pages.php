00e91f4fc7fa4f69af6bb04a47aaaf71c80a61e1
mzwae <mrmonjaie@gmail.com>
2018-02-11 21:59:15 +0000

    added app configuration

---
+++ application/controllers/Pages.php
@@ -0,0 +1,24 @@
+<?php
+
+class Pages extends CI_Controller{
+  public function view($page = 'home'){
+    if (!file_exists(APPPATH . 'view/pages/' . $page . '.php')) {
+      show_404();
+    }
+
+    $data['title'] = ucfirst($page);
+
+    $this->load->view('templates/header');
+    $this->load->view('pages/' . $page, $data);
+    $this->load->view('templates/header');
+
+
+
+
+
+  }
+}
+
+
+
+ ?>
