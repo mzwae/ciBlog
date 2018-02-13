75d074f71e2fe630a481eb061e75b4b86b62297b
mzwae <mrmonjaie@gmail.com>
2018-02-13 19:04:30 +0000

    added posts

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,24 +1,17 @@
 <?php
 
-class Pages extends CI_Controller{
-  public function view($page = 'home'){
-    if (!file_exists(APPPATH . 'views/pages/' . $page . '.php')) {
-      show_404();
+class Pages extends CI_Controller
+{
+    public function view($page = 'home')
+    {
+        if (!file_exists(APPPATH . 'views/pages/' . $page . '.php')) {
+            show_404();
+        }
+
+        $data['title'] = ucfirst($page);
+
+        $this->load->view('templates/header');
+        $this->load->view('pages/' . $page, $data);
+        $this->load->view('templates/footer');
     }
-
-    $data['title'] = ucfirst($page);
-
-    $this->load->view('templates/header');
-    $this->load->view('pages/' . $page, $data);
-    $this->load->view('templates/footer');
-
-
-
-
-
-  }
 }
-
-
-
- ?>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,19 +1,44 @@
 <html>
   <head>
     <title>ciBlog</title>
-    <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
   </head>
-  <body class="container">
-    <nav class="navbar navbar-inverse">
+  <body>
+    <!-- <nav class="navbar navbar-inverse">
       <div class="container">
         <div class="navbar-header">
           <a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
         </div>
         <div id="navbar">
-          <ul class="nav navbar-nav">
-            <li><a href="<?php echo base_url(); ?>">Home</a></li>
-            <li><a href="<?php echo base_url(); ?>about">About</a></li>
+          <ul class="navbar-nav">
+            <li class="nav-item"><a href="<?php echo base_url(); ?>">Home</a></li>
+            <li class="nav-item"><a href="<?php echo base_url(); ?>about">About</a></li>
           </ul>
         </div>
       </div>
-    </nav>
+    </nav> -->
+
+    <nav class="navbar navbar-inverse">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+          <a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
+    </div>
+    <!-- Collection of nav links and other content for toggling -->
+    <div id="navbarCollapse" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+            <li class="nav-item"><a href="<?php echo base_url(); ?>">Home</a></li>
+            <li class="nav-item"><a href="<?php echo base_url(); ?>about">About</a></li>
+        </ul>
+        <!-- <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Login</a></li>
+        </ul> -->
+    </div>
+</nav>
+
+<div class="container">
