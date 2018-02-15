f51ffdfaa6d26869fcfe33768f2cebdb32eb7ff4
mzwae <mrmonjaie@gmail.com>
2018-02-15 15:23:54 +0000

    added create post

--- application/models/Post_model.php
+++ application/models/Post_model.php
@@ -8,12 +8,25 @@ class Post_model extends CI_Model{
 
   public function get_posts($slug = FALSE){
     if ($slug === FALSE) {
+      $this->db->order_by('created_at', 'DESC');
       $query = $this->db->get('posts');
       return $query->result_array();
     }
 
     $query = $this->db->get_where('posts', array('slug' => $slug));
     return $query->row_array();
+  }
+
+  public function create_post(){
+    $slug = url_title($this->input->post('title'));
+
+    $data = array(
+      'title' => $this->input->post('title'),
+      'slug' => $slug,
+      'body' => $this->input->post('body')
+    );
+
+    return $this->db->insert('posts', $data);
   }
 }
 
