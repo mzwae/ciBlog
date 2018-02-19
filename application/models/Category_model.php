30c72e13d1d96d299d3566813980ba79f314832d
mzwae <mrmonjaie@gmail.com>
2018-02-19 20:54:43 +0000

    added categories page

---
+++ application/models/Category_model.php
@@ -0,0 +1,23 @@
+<?php
+
+class category_model extends CI_Model{
+  public function __construct(){
+    $this->load->database();
+  }
+
+  public function create_category(){
+    $data = array(
+      'name' => $this->input->post('name')
+    );
+
+    return $this->db->insert('categories', $data);
+  }
+
+  public function get_categories(){
+    $this->db->order_by('name');
+    $query = $this->db->get('categories');
+    return $query->result_array();
+  }
+}
+
+ ?>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -17,17 +17,19 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
+          <a class="navbar-brand" href="<?=base_url()?>">ciBlog</a>
       </div>
       <!-- Collection of nav links and other content for toggling -->
       <div id="navbarCollapse" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-              <li class="nav-item"><a href="<?php echo base_url(); ?>">Home</a></li>
-              <li class="nav-item"><a href="<?php echo base_url(); ?>about">About</a></li>
-              <li class="nav-item"><a href="<?php echo base_url(); ?>posts">Blog</a></li>
+              <li class="nav-item"><a href="<?=base_url()?>">Home</a></li>
+              <li class="nav-item"><a href="<?=base_url()?>about">About</a></li>
+              <li class="nav-item"><a href="<?=base_url()?>posts">Blog</a></li>
+              <li class="nav-item"><a href="<?=base_url()?>categories">Categories</a></li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
               <li><a href="<?=base_url();?>/posts/create">Creat Post</a></li>
+              <li><a href="<?=base_url();?>/categories/create">Creat Category</a></li>
           </ul>
       </div>
     </div>
