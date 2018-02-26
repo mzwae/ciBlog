f1083539b611a6c9ddb346ab08ad05a5f790d7ba
mzwae <mrmonjaie@gmail.com>
2018-02-26 20:40:29 +0000

    changed baseurl to remote2

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://git.heroku.com/mzapps-ciblog.git';
+$config['base_url'] = 'https://mzapps-ciblog.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
