6451b1873d64fc4656b8831935cbd3296afe2461
mzwae <mrmonjaie@gmail.com>
2019-12-22 23:49:54 +0000

    Refactored code

--- application/controllers/Categories.php
+++ application/controllers/Categories.php
@@ -5,7 +5,6 @@ class Categories extends CI_Controller{
   public function index(){
     $data['title'] = 'Categories';
     $data['categories'] = $this->category_model->get_categories();
-
     $this->load->view('templates/header');
     $this->load->view('categories/index', $data);
     $this->load->view('templates/footer');
@@ -25,10 +24,7 @@ class Categories extends CI_Controller{
       $this->load->view('templates/footer');
     } else {
       $this->category_model->create_category();
-
-      //Set messages
       $this->session->set_flashdata('category_created', 'Your category has been created!');
-
       redirect('categories');
     }
   }
@@ -36,7 +32,6 @@ class Categories extends CI_Controller{
 
   public function posts($id){
     $data['title'] = $this->category_model->get_category($id)->name;
-
     $data['posts'] = $this->post_model->get_posts_by_category($id);
     $this->load->view('templates/header');
     $this->load->view('posts/index', $data);
@@ -49,12 +44,10 @@ class Categories extends CI_Controller{
       if (!$this->session->userdata('logged_in')) {
           redirect('users/login');
       }
-
       $this->category_model->delete_category($id);
 
       //Set messages
       $this->session->set_flashdata('category_deleted', 'Your category has been deleted!');
-
       redirect('categories');
   }
 
